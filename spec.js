322b6714daf690c21a6a93152e5fd5cb3bf13a2e
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-25 18:56:16 -0700

    Define possible test params for current helper functions

--- spec.js
+++ spec.js
@@ -1,3 +1,19 @@
 // DataSimulation.js
 
-getRandomNumOfVisits should return a random integer between 1-20
+getRandomUserId should return a random integer between 1 and 100000
+
+getRandomNumOfVisits should return a random integer between 1 and 30
+
+getRandomNumOfHits should return a random integer between 1 and 25
+
+countUsersByVisits should increment by one at the respective key when a new user is added
+
+getChance should return a random float between 0 and 100
+
+findNumBookingsForMaxTwo should return a number of bookings between 0 and 2
+
+getBookingsForOne should return 1 if chance < 3 and 0 if chance > 3
+
+getBookingsForTwoToSeven should return [appropriate num of bookings];
+
+

c36174030f65a963f9535c70b7c8cfaf449f33e5
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-31 17:49:05 -0700

    Change action_type data type to be be NOT NULL

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE user_hits (
   hit_id                SERIAL UNIQUE NOT NULL PRIMARY KEY,
   user_id               INT NOT NULL,
   visit_num             INT NOT NULL,
-  action_type           VARCHAR(50),
+  action_type           VARCHAR(50) NOT NULL,
   search_id             VARCHAR(50),
   listing_id            INT,
   booking_id            INT UNIQUE REFERENCES bookings(booking_id)

2ae38bbcd9a3e6fa19e32788ec5335e59be78a16
Tiffany Pham <tiffanynpham@gmail.com>
2017-11-01 19:51:32 -0700

    Add functions to consume, publish, and delete messages received from SQS

---
+++ messageBus/amazonSQS.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk');
+const config = require('./config');
+
+AWS.config.update({
+  accessKeyId: 'KEY',
+  secretAccessKey: 'SECRET',
+});
+
+const sqs = new AWS.SQS({ region: config.region });
+const { subscriber } = config;
+
+const MAX_NUMBER_OF_MESSAGES_TO_RECEIVE = 5;
+const MESSAGE_VISIBILITY_TIMEOUT = 10;
+
+
+module.exports.publish = (message) => {
+  const sqsParams = {
+    QueueUrl: subscriber,
+    MessageBody: JSON.stringify(message),
+  };
+
+  sqs.sendMessage(sqsParams, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('success!', data);
+    }
+  });
+};
+
+module.exports.consume = () => {
+  const sqsParams = {
+    QueueUrl: config.queue,
+    AttributeNames: ['All'],
+    MaxNumberOfMessages: MAX_NUMBER_OF_MESSAGES_TO_RECEIVE,
+    VisibilityTimeout: MESSAGE_VISIBILITY_TIMEOUT,
+  };
+
+  return new Promise((resolve, reject) => {
+    sqs.receiveMessage(sqsParams, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+module.exports.done = (message) => {
+  const sqsParams = {
+    QueueUrl: config.queue,
+    ReceiptHandle: message.ReceiptHandle,
+  };
+
+  sqs.deleteMessage(sqsParams, (err) => {
+    if (err) throw err;
+  });
+};

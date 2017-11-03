7ed6cfa998f0d7135a97a7a0431aee09d527df19
Tiffany Pham <tiffanynpham@gmail.com>
2017-11-03 14:56:24 -0700

    Fix data generation script and db queries to properly add to postgresQL

--- database/index.js
+++ database/index.js
@@ -9,13 +9,13 @@ const addUserGeneralActions = (visitsForSearch) => {
     const { visitNum } = visitsForSearch[i];
 
     const hitsForVisit = visitsForSearch[i].hitsDetails.map(hit => `(${userId}, ${visitNum}, '${hit.action}', '${searchId}', ${hit.listingId})`);
-    valuesString += `, ${hitsForVisit.join(', ')}`;
+    valuesString += `,${hitsForVisit.join(',')}`;
   }
 
-  const queryString = `INSERT INTO user_hits (user_id, visit_num, action_type, search_id, listing_id) values ${valuesString}`;
+  const queryString = `INSERT INTO user_hits (user_id, visit_num, action_type, search_id, listing_id) VALUES ${valuesString.substring(1)}`;
 
   dbClient.query(queryString)
-    .then(res => console.log('successfully inserted: ', res))
+    .then(res => console.log('successfully inserted into user_hits: ', res))
     .catch(err => console.error(err));
 
   return visitsForSearch;
@@ -35,15 +35,15 @@ const addBooking = (bookingDetails) => {
 
   const queryString = `with inserted as
     (INSERT INTO bookings (search_id, user_id, visit_num, listing_id, check_in, check_out, total_price, avg_price_per_night)
-      values (${searchId}, ${userId}, ${visitNum}, ${listingId}, '${checkIn}', '${checkOut}', '$${totalPrice}', '$${avgPrice}')
+      VALUES ('${searchId}', ${userId}, ${visitNum}, ${listingId}, '${checkIn}', '${checkOut}', '$${totalPrice}', '$${avgPrice}')
       returning *)
     INSERT INTO user_hits (user_id, visit_num, action_type, search_id, listing_id, booking_id)
-      values (${userId}, ${visitNum}, 'book', '${searchId}', ${listingId}, (select inserted.booking_id from inserted))`;
+      VALUES (${userId}, ${visitNum}, 'book', '${searchId}', ${listingId}, (select inserted.booking_id from inserted))`;
 
   dbClient.query(queryString)
     .then((res) => {
-      console.log('successfully inserted: ', res);
-      messageBus.publishBookingEvent(bookingDetails);
+      console.log('successfully inserted into bookings: ', res);
+      messageBus.publishBookingEvent();
     })
     .catch(err => console.error(err));
 
